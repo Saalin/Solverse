cc364760e5fe1e3a14685d304b15db73ce2af234
Mateusz Calka <saalin@outlook.com>
2020-11-29 17:16:17 +0100

    tests

---
+++ Solverse.Api/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Solverse.Api.Controllers
+{
+    [ApiController]
+    [Route("home")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Solverse.Tests.Api/UnitTest1.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using NUnit.Framework;
+using Solverse.Api;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Solverse.Tests.Api
+{
+    public class Tests
+    {
+        [Test]
+        public async Task Test1()
+        {
+            var factory = new WebApplicationFactory<Startup>();
+            var client = factory.CreateClient();
+
+            var response = await client.GetAsync("/home");
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+    }
+}
