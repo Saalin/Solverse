06b2c24ea2f9cbaf452c3fb83d5db9c978e27f67
Mateusz Calka <saalin@outlook.com>
2020-12-02 22:35:27 +0100

    Net 5

---
+++ Solverse.Domain/Models/Carbon.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Solverse.Domain.Models
+{
+    public abstract class AbstractMolecule
+    {
+        public Guid Id => Guid.NewGuid();
+        public List<Bond> Bonds { get; }
+
+        public AbstractMolecule()
+        {
+            Bonds = new List<Bond>();
+        }
+    }
+
+    public class Bond
+    {
+        public AbstractMolecule A { get; }
+        public AbstractMolecule B { get; }
+
+        public int Rank { get; }
+
+        public Bond(AbstractMolecule a, AbstractMolecule b, int rank = 1)
+        {
+            Rank = rank;
+            A = a;
+            B = b;
+        }
+    }
+
+    public class Carbon : AbstractMolecule
+    {
+    }
+
+    public class Oxygen : AbstractMolecule
+    {
+
+    }
+}
--- Solverse.Tests.Domain/DomainTests.cs
+++ Solverse.Tests.Domain/DomainTests.cs
@@ -20,5 +20,15 @@ namespace Solverse.Tests.Domain
             domainService.CreateExample(name);
             repository.Received().AddExample(Arg.Is<Example>(x => x.Name == name));
         }
+
+        [Test]
+        public void EtenTest()
+        {
+            var c1 = new Carbon();
+            var c2 = new Carbon();
+            var bond = new Bond(c1, c2);
+            c1.Bonds.Add(bond);
+            c2.Bonds.Add(bond);
+        }
     }
 }
